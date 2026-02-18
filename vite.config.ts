3cad493e1d1e5fab9f881c73bff5015abcf56742
Moneta <freyja.intell@gmail.com>
2026-02-19 01:55:11 +0900

    Manual: Trigger new deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import path from 'path';
+import path from 'path'; // Trigger deploy
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
