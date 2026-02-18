e65822d96234501d1e6a0dd325b7de34ca242d9f
Moneta <freyja.intell@gmail.com>
2026-02-19 01:37:40 +0900

    Debug: Add global error alert

--- index.tsx
+++ index.tsx
@@ -2,6 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+
+// --- DEBUGGING: Global Error Handler ---
+window.onerror = function (message, source, lineno, colno, error) {
+  alert('Error: ' + message + '\nLine: ' + lineno + '\nColumn: ' + colno + '\nStack: ' + (error ? error.stack : 'N/A'));
+};
+window.onunhandledrejection = function (event) {
+  alert('Unhandled Rejection: ' + event.reason);
+};
+// ---------------------------------------
+
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
